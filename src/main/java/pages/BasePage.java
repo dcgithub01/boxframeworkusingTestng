6a089a3dd50add25091c166ae845a58ef43b0bad
deepak <h2so4in2008@gmail.com>
2022-06-02 19:48:33 +0530

    added threadLocal,RemoteWebDriver,Parallel execution logic

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -4,15 +4,21 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
-import  java.util.concurrent.*;
+import java.util.concurrent.*;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.aventstack.extentreports.utils.FileUtil;
 
@@ -22,33 +28,59 @@ public class BasePage {
 
 	public WebDriver driver;
 	public Properties prop;
-	
+	public static ThreadLocal<WebDriver> tl = new ThreadLocal<WebDriver>();
+
 	@SuppressWarnings("deprecation")
-	public WebDriver init_driver(String browserName)
-	{
-		if(browserName.equalsIgnoreCase("Chrome"))
-		{
+	public WebDriver init_driver(String browserName) {
+		if (browserName.equalsIgnoreCase("Chrome")) {
 			WebDriverManager.chromedriver().setup();
-			driver= new ChromeDriver();
-		}
-		else if(browserName.equalsIgnoreCase("firefox"))
-		{
+			// driver= new ChromeDriver();
+			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+				try {
+					
+					ChromeOptions co = new ChromeOptions();
+					co.setCapability("browserName", "chrome");
+
+					tl.set(new RemoteWebDriver(new URL(prop.getProperty("remoteURL")), co));
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				tl.set(new ChromeDriver());
+			}
+		} else if (browserName.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
-			driver= new FirefoxDriver();
+			// driver= new FirefoxDriver();
+			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+				try {
+
+					FirefoxOptions fo = new FirefoxOptions();
+					fo.setCapability("browserName", "firefox");
+					tl.set(new RemoteWebDriver(new URL(prop.getProperty("remoteURL")), fo));
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+
+				tl.set(new FirefoxDriver());
+			}
 		}
+			getdriver().manage().window().maximize();
+			getdriver().manage().timeouts().implicitlyWait(10, java.util.concurrent.TimeUnit.SECONDS);
+			getdriver().manage().deleteAllCookies();
+			getdriver().get("https://app.box.com/login");
+			return getdriver();
 		
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, java.util.concurrent.TimeUnit.SECONDS);
-		driver.manage().deleteAllCookies();
-		 driver.get("https://app.box.com/login");
-		return driver;
-		
-			
 	}
-	
-	public Properties init_prop()
-	{
-		prop= new Properties();
+
+	public synchronized WebDriver getdriver() {
+		return tl.get();
+	}
+
+	public Properties init_prop() {
+		prop = new Properties();
 		FileInputStream ip;
 		try {
 			ip = new FileInputStream("src/test/resources/config/config.properties");
@@ -60,15 +92,14 @@ public class BasePage {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
+
 		return prop;
-		
-		
+
 	}
-	public String getScreenshot()
-	{
-		File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		String dest_path= System.getProperty("user.dir")+"Screenshots"+System.currentTimeMillis()+".png";
+
+	public String getScreenshot() {
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String dest_path = System.getProperty("user.dir") + "Screenshots" + System.currentTimeMillis() + ".png";
 		File destination = new File(dest_path);
 		try {
 			FileUtils.copyFile(src, destination);
